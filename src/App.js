2178d40da4cce599048e8aa368a79aaeeed4e35f
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2019-01-04 21:20:37 +0000

    basic layout is ok and working, tutorial is fine so far

--- src/App.js
+++ src/App.js
@@ -30,40 +30,23 @@ class App extends Component {
       isOpen: !this.state.isOpen
     })
   }
+  
   render() {
     return (
       <div className="App">
         <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarBrand href="/">TrafficMap</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/components/">Components</NavLink>
+                1,000 Traffic Incidents Today
               </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
             </Nav>
           </Collapse>
         </Navbar>
+
+        <div id="map"></div>
       </div>
     );
   }
