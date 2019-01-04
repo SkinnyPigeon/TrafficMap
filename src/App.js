9c2bc9339b1d8394f7fd37d1fe257873877765eb
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2019-01-04 21:55:32 +0000

    heroku didn't workm removing yarn lock let it upload but I think server needs to be declared somewhere. Hopefully that'll be covered

--- src/App.js
+++ src/App.js
@@ -1,50 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem 
-} from 'reactstrap';
 
+import Header from './components/Header'
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    }
+    this.state = {}
   }
 
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
-  }
-  
   render() {
     return (
       <div className="App">
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">TrafficMap</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                1,000 Traffic Incidents Today
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+        <Header />
 
         <div id="map"></div>
       </div>
