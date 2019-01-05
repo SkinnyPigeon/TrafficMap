f8d86b9a697c7f99ba2431599118af87b4f9150f
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2019-01-05 00:27:51 +0000

    adding some markers using the map function

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import ReactMapGL from 'react-map-gl';
+import MapGL, {Marker} from 'react-map-gl';
+import Pin from './Pin';
 
 export default class Map extends Component {
     constructor(props) {
@@ -14,16 +15,27 @@ export default class Map extends Component {
                 longitude: -113.4909,
                 pixelRatio: 3
             },
+            coords: [
+                {latitude: 53.5225, longitude: -113.6242},
+                {latitude: 53.5437, longitude: -113.4947},
+                {latitude: 53.5439, longitude: -113.4914},
+            ]
         } 
     }
 
     render(){
+        const {coords} = this.state;
         return (
-            <ReactMapGL
+            <MapGL
                 mapboxApiAccessToken = {process.env.REACT_APP_MAPBOX_TOKEN}
                 {...this.state.viewport}
-                onViewportChange={(viewport) => this.setState({viewport})}
-            />
+                onViewportChange={(viewport) => this.setState({viewport})}>
+                {coords.map(coord => (
+                    <Marker latitude={coord.latitude} longitude={coord.longitude}>
+                        <Pin />
+                    </Marker>
+                ))}
+            </MapGL>
         );
     }
 }
