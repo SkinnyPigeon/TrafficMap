178ccef75540896d52da3689b46be7a60e8854ca
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2019-01-05 14:09:27 +0000

    christ that was painful. still not 100% on what the issue was there but fixed it

--- src/App.js
+++ src/App.js
@@ -1,67 +1,17 @@
-import React, { Component } from 'react';
-import {Container} from 'reactstrap';
-import Map from './components/Map';
-import Header from './components/Header'
+import {connect} from 'react-redux';
+import Home from './Home'
+import { fetchFromAPI } from './store/actions/api_actions';
 
-import './App.css';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      applicationName: 'TrafficMaps',
-      data: null,
-      api_url: 'https://data.edmonton.ca/resource/ju4q-wijd.json'
-    }
-  }
-
-  componentDidMount() {
-    const {data, api_url} = this.state;
-
-    if(!data) {
-      fetch(api_url, {method: 'GET'})
-      .then(response => response.json())
-      .then(response => this.createFeatureCollection(response))
-      .then(response => this.setState({data: response}))
-    }
-  }
-
-  createFeatureCollection(data) {
-    let features = [];
-    data.forEach(point => {
-        features.push({
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    parseFloat(point.location.coordinates[0]),
-                    parseFloat(point.location.coordinates[1])
-                ]
-            },
-            "properties": {
-                "description": point.description,
-                "details": point.details,
-                "duration": point.duration,
-                "impact": point.impact
-            }
-        });
-    })
+const mapStateProps = state => {
     return {
-        "type": "FeatureCollection",
-        "features": features
-    } 
-  }
-  render() {
-    return (
-      <div className="App">
-        <Header appName = {this.state.applicationName}/>
-        <Container>
-          <Map data={this.state.data}/>
-        </Container>
-      </div>
-    );
-  }
+        API: state.APIReducer
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        startFetch: () => dispatch(fetchFromAPI())
+    }
 }
 
-export default App;
+export default connect(mapStateProps, mapDispatchToProps)(Home);
---
+++ src/Home.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {Container} from 'reactstrap';
+import Map from './components/Map';
+import Header from './components/Header'
+
+import './App.css';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      applicationName: 'TrafficMaps'
+    }
+  }
+
+  componentDidMount() {
+    const {startFetch, API: {data}} = this.props;
+    if(!data) startFetch();
+  }
+
+  render() {
+    const { data } = this.props.API;
+    const count =  data && data.features.length;
+    return (
+      <div>
+        <Header count={count} appName={this.state.applicationName} />
+
+        <Container>
+          <Map data={data} />
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Home;
